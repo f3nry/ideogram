fe4fa09415d61bdfa0d4b9a6cff4b87d3a87ec13
Wanelo <wanelo@Paul-Henrys-MacBook-Pro.local>
2012-07-28 20:23:04 -0700

    fixing syntax error

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
-io.set('heartbeat interval', 30);
-
 io = sio.listen(
   app.listen(port, function () {
     console.log('   app listening on http://localhost:' + port);
   })
 );
+
+io.set('heartbeat interval', 30);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
